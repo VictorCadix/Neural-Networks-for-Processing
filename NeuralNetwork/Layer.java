ee8c094486497d2a8003a0f685bbc2dc64c9f710
David <david.abadp@alumnos.upm.es>
2022-09-05 19:18:05 +0200

    Vuelta como antes

--- NeuralNetwork/Layer.java
+++ NeuralNetwork/Layer.java
@@ -22,7 +22,7 @@ public class Layer{
     this.lay_type = layer_type;
     this.activ_type = activation_type;
     this.nNeurons = nNeu;
-    weights = new float[nNeu][prevLayer.nNeurons+1];
+    weights = new float[nNeu][prevLayer.nNeurons];
     neurons = new float [nNeu];
     init();
     nParameters = weights.length * weights[0].length;
@@ -37,7 +37,7 @@ public class Layer{
   
   public void init(){
     for (int i = 0; i < nNeurons; i++){
-      for (int j = 0; j < prevLayer.nNeurons+1; j++){
+      for (int j = 0; j < prevLayer.nNeurons; j++){
         weights[i][j] = parent.random((float)-1.0,(float) 1.0);
       }
     }
@@ -47,7 +47,7 @@ public class Layer{
     //inputs x Weights (+ bias)
     for (int i = 0; i < nNeurons; i++){
       float sum = 0;
-      for (int j = 0; j < prevLayer.nNeurons+1; j++){
+      for (int j = 0; j < prevLayer.nNeurons; j++){
         if(j != prevLayer.nNeurons){
           sum += weights[i][j] * prevLayer.neurons[j];
         }
